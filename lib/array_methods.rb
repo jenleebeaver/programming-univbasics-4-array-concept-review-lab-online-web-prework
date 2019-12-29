3f54c535c7a185dd8c80a2d7b0450d864472e738
Jennifer Beaver <jen.lee.beaver@gmail.com>
2019-12-29 15:50:40 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -9,7 +9,11 @@ end
 
 
 def find_max_value(array)
-  # Add your solution here
+  counter = 0 
+  while counter < array.length do 
+    if array[counter] > 4
+      return counter 
+  counter += 1
 end
 
 def find_min_value(array)
