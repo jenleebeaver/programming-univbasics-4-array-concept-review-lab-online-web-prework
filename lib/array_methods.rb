cfa479d23972edae560727e71326ad1f0011037a
Jennifer Beaver <jen.lee.beaver@gmail.com>
2019-12-29 15:54:37 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,25 @@
+
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  counter = 0
+  while counter < array.length do
+    if array[counter] = value_to_find
+      return counter
+  counter += 1
 end
 
+
 def find_max_value(array)
-  # Add your solution here
+  counter = 0
+  while counter < array.length do
+    if array[counter] > 4
+      return counter
+  counter += 1
 end
 
 def find_min_value(array)
-  # Add your solution here
+  counter = 0
+  while counter < array.length do
+    if array[counter] = 1
+      return counter
+  counter += 1
 end
